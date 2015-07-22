df76989732968792525628206b14854aedd0d13e
Steve Lamb <stevel@richinteractive.com>
2015-07-22 13:39:47 +0100

    added dump functionality to dump page json to logs folder

---
+++ routes/dump.js
@@ -0,0 +1,26 @@
+
+var express = require('express');
+var router = express.Router();
+var _ = require('underscore');
+var fs = require('fs');
+
+router.post('/', 
+	function(request, response, next){
+		var object = request.body;
+		
+		console.log('dump');
+		console.log(object);
+	
+		fs.writeFile("../logs/lastpage.log", JSON.stringify(object, null, 4), 
+			function(err) {
+		    	if(err) {
+		        	return console.log(err);
+		    	}
+		    	console.log("The file was saved!");
+				response.send('ok');
+			}
+		);
+	}
+); 	
+
+module.exports = router;
