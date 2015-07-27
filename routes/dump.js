c928f0e8947383776e93ae69300e9aba64dea891
Steve Lamb <stevel@richinteractive.com>
2015-07-27 12:34:56 +0100

    added ability to specify a dump filename e.g. /api/dump/filename

--- routes/dump.js
+++ routes/dump.js
@@ -4,31 +4,35 @@ var router = express.Router();
 var _ = require('underscore');
 var fs = require('fs');
 
+function dumpFile(fileName, object){
+	fs.unlink(fileName,
+		function(){
+			fs.writeFile(fileName, JSON.stringify(object, null, 4), 
+				function(err) {
+			    	if(err) {
+			        	return console.log(err);
+			    	}
+				}
+			);
+		}
+	);
+}
+
 router.post('/', 
 	function(request, response, next){
 		var object = request.body;
-		
-		console.log('dump');
-		console.log(object);
-	
-		fs.writeFile("../logs/lastpage.log", JSON.stringify(object, null, 4), 
-			function(err) {
-		    	if(err) {
-		        	return console.log(err);
-		    	}
-		    	console.log("The file was saved!");
-				fs.writeFile("../bubl/www/lastpage.json", JSON.stringify(object, null, 4), 
-					function(err) {
-				    	if(err) {
-				        	return console.log(err);
-				    	}
-				    	console.log("The file was saved!");
-						response.send('ok');
-					}
-				);
-			}
-		);
+		dumpFile('../bubl/www/lastpage.json', object);		
+		response.send('ok');
 	}
 ); 	
+
+router.post('/:dump_file', 
+	function(request, response, next){
+		var object = request.body;
+		dumpFile('../bubl/www/' + request.params.dump_file + '.json', object);		
+		response.send('ok');
+	}
+); 	
+
 
 module.exports = router;
