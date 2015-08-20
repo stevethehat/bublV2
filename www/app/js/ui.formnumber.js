1b726140e7f0592308ba724d154c13c68e24ad48
Steve Lamb <stevel@richinteractive.com>
2015-08-20 18:24:51 +0100

    new form number control

---
+++ www/app/js/ui.formnumber.js
@@ -0,0 +1,58 @@
+/// <reference path="../../typings/jquery.d.ts"/>
+
+"use strict";
+
+var ZEN = (function (ZEN, _, $) {
+	ZEN.namespace('ui');
+	_.extend(ZEN.ui, (function () {
+
+		
+		function FormNumber (params, parent) {
+			if (arguments.length > 0) {
+				ZEN.ui.Control.call(this, params, parent);
+			}
+			return this;
+		}
+
+		FormNumber.prototype = new ZEN.ui.Control();
+		
+		_.extend(
+			FormNumber.prototype,
+			{
+				init: function (params, parent) {
+					// call the base class init method
+					ZEN.ui.Control.prototype.init.call(this, params, parent);
+				},
+				
+				label: function () {
+				},
+				
+				getElement: function () {
+					if (this.el === null) {
+						ZEN.ui.Base.prototype.getElement.call(this);
+						this.el.addClass('zen-formnumber');
+						var container = $('<div/>').addClass('formElementContainer').appendTo(this.el);
+						var label = $('<label>' + this.params.label + '</label>').appendTo(container);
+						var edit = $('<input type="number"/>')
+							.attr('data-source', this.params.source)
+							.attr('data-type', 'Number')
+							.attr('value', this.params.value)
+							.appendTo(container);
+							
+						this.resize();
+					}
+					return this.el;
+				}
+			}
+		);
+
+		ZEN.registerType('FormNumber', FormNumber);
+
+		return {
+			FormNumber: FormNumber
+		};
+		
+
+	}()));
+	return ZEN;
+}(ZEN || {}, _, $));
