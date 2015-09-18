c4b84e338abf71118848df41e228507fe3272c11
Steve Lamb <stevel@richinteractive.com>
2015-09-18 15:07:06 +0100

    added filters to images..

--- www/app/js/ui.bublimage.js
+++ www/app/js/ui.bublimage.js
@@ -25,13 +25,57 @@ var ZEN = (function (ZEN, _, $) {
 					ZEN.ui.BublControl.prototype.init.call(this, params, parent);
 					ZEN.events.buttonHandler (this, this.el);
 				},
+				
+				setInactiveFilter: function(){
+					var self = this;
+					try{
+						if(self.getSetting(this.params.content.inactivefilter, 'none') !== 'none'){
+							this.el.css('-webkit-filter', this.params.content.inactivefilter + '(' + this.params.content.inactivefiltervalue + '%)');
+						}
+					} catch(error){
+						
+					}						
+				},
+				
+				setActiveFilter: function(){
+					var self = this;
+					try{
+						if(self.getSetting(this.params.content.activefilter, 'none') !== 'none'){
+							this.el.css('-webkit-filter', this.params.content.activefilter + '(' + this.params.content.activefiltervalue + '%)');
+						} else {
+							this.el.css('-webkit-filter', '');
+						}
+					} catch(error){
+						
+					}						
+				},
+
+				notify: function (message) {
+					var self = this;
+					message.source = this;
+					
+					if(bublApp.displayMode === 'app'){
+						if(message.type === 'active') {
+							ZEN.notify ("ui.bublcontrol", message);
+						}
+					} else {
+						if (message.type === 'inactive') {
+							self.setInactiveFilter();
+						}
+						if (message.type === 'highlight') {
+							self.setActiveFilter();
+						}
+					}
+				},
+				
 				getElement: function () {
 					var self = this;
 					if (this.el === null) {
 						ZEN.ui.Base.prototype.getElement.call(this);
 						this.el.addClass('zen-image');
 						self.setupStylingDiv();
-						
+						self.setInactiveFilter();
+												
 						if(this.params.content && this.params.content.url !== undefined){
 							//this.el.html('<img width="' + this.parent.el.width() + '" height="' + this.parent.el.height() + '" src="' + this.params.content.url + '"/>');
 							self.stylingDiv.html('<img width="100%" height="100%" src="' + this.params.content.url + '"/>');
