f267433b344802d38d40fb32aa3adfc7dbfe422e
Steve Lamb <stevel@richinteractive.com>
2015-09-18 15:07:44 +0100

    added support for min/max + value on the end of the label

--- www/app/js/ui.formrange.js
+++ www/app/js/ui.formrange.js
@@ -28,17 +28,31 @@ var ZEN = (function (ZEN, _, $) {
 				},
 				
 				getElement: function () {
+					var self = this;
 					if (this.el === null) {
 						ZEN.ui.Base.prototype.getElement.call(this);
 						this.el.addClass('zen-formedit');
 						var container = $('<div/>').addClass('formElementContainer').appendTo(this.el);
-						var label = $('<label>' + this.params.label + '</label>').appendTo(container);
+						var label = $('<label>' + this.params.label + ' ' + this.params.value + '</label>').appendTo(container);
 						var edit = $('<input type="range"/>')
 							.attr('data-source', this.params.source)
 							.attr('data-type', 'Number')
 							.attr('value', this.params.value)
 							.appendTo(container);
 							
+						if(this.params.min !== undefined && this.params.min !== null){
+							edit.attr('min', this.params.min);
+						}	
+						if(this.params.max !== undefined && this.params.max !== null){
+							edit.attr('max', this.params.max);
+						}	
+						
+						edit.on('change',
+							function(){
+								label.text(self.params.label + ' ' + edit.val());
+							}
+						);
+						
 						this.resize();
 					}
 					return this.el;
