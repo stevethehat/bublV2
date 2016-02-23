6e0a85c7de19a0ca4f2035d7c11e1e8543df88cd
Steve Lamb <stevel@richinteractive.com>
2016-02-23 14:28:03 +0000

    attempt at text dumping

--- routes/dump.js
+++ routes/dump.js
@@ -18,6 +18,20 @@ function dumpFile(fileName, object){
 	);
 }
 
+function dumpText(fileName, text){
+	fs.unlink(fileName,
+		function(){
+			fs.writeFile(fileName, text, 
+				function(err) {
+			    	if(err) {
+			        	return console.log(err);
+			    	}
+				}
+			);
+		}
+	);
+}
+
 router.post('/', 
 	function(request, response, next){
 		var object = request.body;
@@ -25,6 +39,15 @@ router.post('/',
 		response.send('ok');
 	}
 ); 	
+
+router.post('/:dump_file/astext', 
+	function(request, response, next){
+		var text = request.body;
+		dumpText('../bubl/www/dumps/' + request.params.dump_file, text);		
+		response.send('ok');
+	}
+); 	
+
 
 router.post('/:dump_file', 
 	function(request, response, next){
